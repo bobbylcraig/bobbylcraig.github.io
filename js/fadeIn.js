2ef631a4eb213399e472ddada77d30c861dc8a21
Bobby Craig <robtcraig@gmail.com>
2016-01-09 04:30:08 -0500

    new primary homepage added

---
+++ js/fadeIn.js
@@ -0,0 +1,31 @@
+var intro = false;
+$(document).ready(function() {
+    $(".open-window").hide();
+    if (intro) {
+        $(".open-window").show();
+        $(".main-content").hide();
+        $(".first").hide();
+        $(".second").hide();
+        $(".third").hide();
+        $(".fourth").hide();
+        $(".first").fadeIn("slow",function() {
+            $(".first").delay(3000).fadeOut("slow", function() {
+                $(".second").delay(1000).fadeIn("slow", function() {
+                    $(".second").delay(3000).fadeOut("slow", function() {
+                        $(".third").delay(1000).fadeIn("slow", function() {
+                            $(".third").delay(3000).fadeOut("slow", function() {
+                                $(".fourth").delay(1000).fadeIn("slow", function() {
+                                    $(".fourth").delay(3000).fadeOut("slow", function() {
+                                        $(".open-window").delay(1000).fadeOut("slow", function() {
+                                            $(".main-content").fadeIn("slow");
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+});
