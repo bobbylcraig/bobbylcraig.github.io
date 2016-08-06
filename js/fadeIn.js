10caf5019e8c0a70f58e23518d8cc9122a339a2a
Bobby Craig <robtcraig@gmail.com>
2016-08-05 23:16:09 -0400

    Back to the old site

---
+++ js/fadeIn.js
@@ -0,0 +1,23 @@
+var intro = true;
+$(document).ready(function() {
+    $(".open-window").hide();
+    if (intro) {
+        $(".open-window").show();
+        $(".main-content").hide();
+        $(".first").hide();
+        $(".second").hide();
+        $(".third").hide();
+        $(".fourth").hide();
+        $(".first").fadeIn("slow",function() {
+            $(".first").delay(3000).fadeOut("slow", function() {
+                $(".second").delay(1000).fadeIn("slow", function() {
+                    $(".second").delay(3000).fadeOut("slow", function() {
+                        $(".open-window").delay(1000).fadeOut("slow", function() {
+                            $(".main-content").fadeIn("slow");
+                        });
+                    });
+                });
+            });
+        });
+    }
+});
